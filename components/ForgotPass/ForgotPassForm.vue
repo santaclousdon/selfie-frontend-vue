4077597dde9068220e3cb0ab68333e69fb1deb9e
santaclousdon <santaclous112@gmail.com>
2024-03-05 08:54:56 -0800

    reset password functionality COMPLETED!

--- components/ForgotPass/ForgotPassForm.vue
+++ components/ForgotPass/ForgotPassForm.vue
@@ -1,9 +1,19 @@
 <template>
   <section class="">
     <div class="container login-section">
-      <div class="col-sm-6 alert alert-danger" role="alert">
-        <span class="mr-3"><img src="../../assets/images/warningIcon.png" alt=""></span>
-        You have entered an incorrect email address.
+      <div class="col-sm-6 alert alert-danger" role="alert" v-if="alert">
+        <div class="row aligin-items-center">
+          <div class="col-md-11 col-lg-11 col-sm-11"><img src="../../assets/images/warningIcon.png" alt="">
+            {{ message }}
+          </div>
+          <div class="col-lg-1 col-md-1 col-sm-1 text-right"><span class="text-right alert-close" @click="closeAlert"> <i
+                class="fa fa-times"></i> </span></div>
+        </div>
+      </div>
+      <div class="col-sm-6 alert alert-success" role="alert" v-if="successalert">
+        <span class="mr-3"><img src="../../assets/images/pending.png" alt=""></span>
+        {{ message }}
+        <span class="text-right alert-close" @click="closeAlert"> <i class="fa fa-times"></i> </span>
       </div>
       <div class="row align-items-center justify-content-center">
         <div class="col-sm-6 login-form p-5">
@@ -11,18 +21,18 @@
             Forgot password
           </h2>
           <div class="response-output" />
-          <form action="#" method="post">
+          <form @submit.prevent="HandleReset">
             <div class="contact-form">
               <div class="details">
                 <p>Forgot your password? Don’t worry! Resetting your
                   password is easy. Just type in the email you registered to Selfie.cash.</p>
               </div>
-              <div> <input type="text" name="your-email" class="form-control text " placeholder="Your Email"> </div>
+              <div> <input type="email" v-model="email" name="your-email" class="form-control text "
+                  placeholder="Your Email"> </div>
               <div>
                 <div class="row justify-content-center pt-3 ">
                   <div class="col-lg-6 col-md-6 col-sm-12 text-right align-items-center">
-                    <button id="submitLogin" click="/contact-us" name="submit" type="submit" value="Send"
-                      class="button blue-btn btn-sm d-block">
+                    <button name="submit" type="submit" class="button blue-btn btn-sm d-block">
                       Reset password
                     </button>
                   </div>
@@ -36,8 +46,51 @@
   </section>
 </template>
 <script>
+import { axios } from '~/nuxt.config';
+
 export default {
-  name: 'ForgotPassForm'
+  name: 'ForgotPassForm',
+  data() {
+    return {
+      message: "",
+      alert: false,
+      successalert: false,
+      email: ""
+    };
+  },
+  methods: {
+    closeAlert() {
+      this.alert = false
+      this.successalert = false
+    },
+    async HandleReset() {
+      try {
+
+        if (!this.email) {
+          this.message = "Please type your email."
+          this.alert = true
+          return
+        }
+
+        const res = await this.$axios.$post("/api/auth/passwordreset", {
+          email: this.email
+        });
+
+        const resetEmail = await this.$axios.$post("/api/auth/resetEmail", {
+          password: res.password,
+          email: this.email
+        })
+
+        this.message = res.message
+        this.successalert = true
+
+      } catch (error) {
+          this.message = "Please type your email registered in selfie.cash"
+          this.alert = true
+      }
+
+    }
+  }
 }
 </script>
 <style scoped >
@@ -121,6 +174,7 @@ input.text {
   border-radius: 9999px;
   padding: 2px 5px;
 }
+
 .alert {
   margin-left: auto;
   margin-right: auto;
@@ -130,6 +184,14 @@ input.text {
   font-family: Montserrat;
   font-weight: 500;
   word-wrap: break-word
+}
+
+.alert-close {
+  float: right;
+  cursor: pointer;
+}
+.alert-success {
+  background: rgb(0, 172, 92);
 }
 </style>
 
