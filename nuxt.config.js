e2e91e256579d5526e8b60a55ba78065341694d8
santaclousdon <santaclous112@gmail.com>
2024-04-02 16:17:33 -0700

    production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-  mode: 'universal',
+  // mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Selfie Cash',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
